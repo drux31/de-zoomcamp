72bfcb61905a57251d76443f0627c9306d1e9960
landryx <landryx@gmail.com>
2023-06-08 22:41:02 +0200

    Building ETL orchestration with prefect

--- week_2_workflow_orchestration/etl_w_gcpn_prefect/parameterize_flow.py
+++ week_2_workflow_orchestration/etl_w_gcpn_prefect/parameterize_flow.py
@@ -1,13 +1,13 @@
 from pathlib import Path
 import pandas as pd
 from prefect import flow, task
-#from prefect_gcp.cloud_storage import GcsBucket
-#from random import randint
+from prefect.tasks import task_input_hash
+from datetime import timedelta
 
 from prefect_gcp.cloud_storage import GcsBucket
 gcp_cloud_storage_bucket_block = GcsBucket.load("zoom-gcs")
 
-@task(log_prints=True, retries=3)
+@task(log_prints=True, retries=3, cache_key_fn=task_input_hash, cache_expiration=timedelta(days=1))
 def fetch(dataset_url: str) -> pd.DataFrame:
     """Read data from web into pandas DataFrame"""
     #if randint(0, 1) > 0:
@@ -45,11 +45,8 @@ def write_gcs(path: Path) -> None:
 
 
 @flow()
-def etl_web_to_gcs() -> None:
+def etl_web_to_gcs(year: int, month: int, color: str) -> None:
     """ Thuis is the main ETL function """
-    color = "yellow"
-    year = 2021
-    month = 1
     dataset_file = f"{color}_tripdata_{year}-{month:02}"
     dataset_url = f"https://github.com/DataTalksClub/nyc-tlc-data/releases/download/{color}/{dataset_file}.csv.gz"
 
@@ -58,5 +55,15 @@ def etl_web_to_gcs() -> None:
     path = write_local(df_clean, color, dataset_file)
     write_gcs(path)
 
+
+@flow(log_prints=True)
+def etl_parent_flow(months: list[int] = [1, 2], year: int = 2021, color: str = "yellow") -> None :
+    for month in months:
+        etl_web_to_gcs(year, month, color)
+    
+
 if __name__ == '__main__':
-    etl_web_to_gcs()
+    color = "yellow"
+    months = [1, 2, 3]
+    year = 2021
+    etl_parent_flow(months, year, color)
