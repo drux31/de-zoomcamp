07ecfcd7e2e3d982ee88d60a60ec7cb0c1ea7ede
landryx <landryx@gmail.com>
2023-06-16 21:20:25 +0200

    Dag GCS to BQ with Airflow

--- week_2_workflow_orchestration/airflow/dags/data_ingestion_dag.py
+++ week_2_workflow_orchestration/airflow/dags/data_ingestion_dag.py
@@ -8,10 +8,11 @@ from airflow.operators.python import PythonOperator
 
 from google.cloud import storage
 from airflow.providers.google.cloud.operators.bigquery import BigQueryCreateExternalTableOperator
-from airflow.providers.google.cloud.transfers.gcs_to_bigquery import GCSToBigQueryOperator
-import pyarrow.csv as pv
+
+#import pyarrow.csv as pv
 import pyarrow.parquet as pq
-import gzip
+import pandas as pd
+import pyarrow as pa
 
 
 PROJECT_ID = os.environ.get("GCP_PROJECT_ID")
@@ -19,23 +20,31 @@ BUCKET = os.environ.get("GCP_GCS_BUCKET")
 color = "yellow"
 year = 2021
 month = 1
-dataset_file = f"{color}_tripdata_{year}-{month:02}"
+dataset_file = f"{color}_tripdata_{year}-{month:02}.csv.gz"
 
 
 #Fetch the data
-dataset_url = f"https://github.com/DataTalksClub/nyc-tlc-data/releases/download/{color}/{dataset_file}.csv.gz"
+dataset_url = f"https://github.com/DataTalksClub/nyc-tlc-data/releases/download/{color}/{dataset_file}"
+
 
 path_to_local_home = os.environ.get("AIRFLOW_HOME", "/opt/airflow/")
 parquet_file = dataset_file.replace('.csv.gz', '.parquet')
 
-BIGQUERY_DATASET = os.environ.get("BIGQUERY_DATASET", 'trips_data_all')
+#BigQuery dataset
+BQ_DATASET = os.environ.get("BIGQUERY_DATASET", 'trips_data_all')
 
 
 def format_to_parquet(src_file):
     if not src_file.endswith('.csv.gz'):
         logging.error("Can only accept source files in CSV format, for the moment")
         return
-    table = pv.read_csv(src_file)
+    #create a dataframe for unzipping the csv file
+    df = pd.read_csv(src_file, compression='gzip')
+
+    # Convert DataFrame to Apache Arrow Table
+    table = pa.Table.from_pandas(df)
+
+    # Write the table to parquet format.
     pq.write_table(table, src_file.replace('.csv.gz', '.parquet'))
 
 
@@ -101,14 +110,14 @@ with DAG(
             "local_file": f"{path_to_local_home}/{parquet_file}",
         },
     )
-
-    """
+    
+    
     bigquery_external_table_task = BigQueryCreateExternalTableOperator(
         task_id="bigquery_external_table_task",
         table_resource={
             "tableReference": {
                 "projectId": PROJECT_ID,
-                "datasetId": BIGQUERY_DATASET,
+                "datasetId": BQ_DATASET,
                 "tableId": "external_table",
             },
             "externalDataConfiguration": {
@@ -117,81 +126,5 @@ with DAG(
             },
         },
     )
-    """
-    """
-    bigquery_external_table_task = BigQueryCreateExternalTableOperator(
-        task_id="bigquery_external_table_task",
-        destination_project_dataset_table=f"{BIGQUERY_DATASET}.external_table",
-        bucket=BUCKET,
-        source_objects= [f"gs://{BUCKET}/raw/{parquet_file}"],
-         schema_fields=[
-        {"name": "VendorID", "type": "INTEGER", "mode": "REQUIRED"},
-        {"name": "tpep_pickup_datetime", "type": "TIMESTAMP", "mode": "NULLABLE"},
-        {"name": "tpep_dropoff_datetime", "type": "TIMESTAMP", "mode": "NULLABLE"},
-        {"name": "passenger_count", "type": "INTEGER", "mode": "NULLABLE"},
-        {"name": "trip_distance", "type": "FLOAT", "mode": "NULLABLE"},
-        {"name": "RatecodeID", "type": "FLOAT", "mode": "NULLABLE"},
-        {"name": "store_and_fwd_flag", "type": "STRING", "mode": "NULLABLE"},
-        {"name": "PULocationID", "type": "INTEGER", "mode": "NULLABLE"},
-        {"name": "DOLocationID", "type": "INTEGER", "mode": "NULLABLE"},
-        {"name": "payment_type", "type": "INTEGER", "mode": "NULLABLE"},
-        {"name": "fare_amount", "type": "FLOAT", "mode": "NULLABLE"},
-        {"name": "extra", "type": "FLOAT", "mode": "NULLABLE"},
-        {"name": "mta_tax", "type": "FLOAT", "mode": "NULLABLE"},
-        {"name": "tip_amount", "type": "FLOAT", "mode": "NULLABLE"},
-        {"name": "tolls_amount", "type": "FLOAT", "mode": "NULLABLE"},
-        {"name": "improvement_surcharge", "type": "FLOAT", "mode": "NULLABLE"},
-        {"name": "total_amount", "type": "FLOAT", "mode": "NULLABLE"},
-        {"name": "congestion_surcharge", "type": "FLOAT", "mode": "NULLABLE"},
-    ],
-    )
-    """
-
-    bigquery_external_table_task = BigQueryCreateExternalTableOperator(
-        task_id="bigquery_external_table_task",
-        table_resource={
-            "tableReference": {
-                "projectId": PROJECT_ID,
-                "datasetId": BIGQUERY_DATASET,
-                "tableId": "external_table",
-            },
-            "schema": {
-                "fields": [
-                    {"name": "VendorID", "type": "INTEGER", "mode": "REQUIRED"},
-                    {"name": "tpep_pickup_datetime", "type": "TIMESTAMP", "mode": "NULLABLE"},
-                    {"name": "tpep_dropoff_datetime", "type": "TIMESTAMP", "mode": "NULLABLE"},
-                    {"name": "passenger_count", "type": "INTEGER", "mode": "NULLABLE"},
-                    {"name": "trip_distance", "type": "FLOAT", "mode": "NULLABLE"},
-                    {"name": "RatecodeID", "type": "FLOAT", "mode": "NULLABLE"},
-                    {"name": "store_and_fwd_flag", "type": "STRING", "mode": "NULLABLE"},
-                    {"name": "PULocationID", "type": "INTEGER", "mode": "NULLABLE"},
-                    {"name": "DOLocationID", "type": "INTEGER", "mode": "NULLABLE"},
-                    {"name": "payment_type", "type": "INTEGER", "mode": "NULLABLE"},
-                    {"name": "fare_amount", "type": "FLOAT", "mode": "NULLABLE"},
-                    {"name": "extra", "type": "FLOAT", "mode": "NULLABLE"},
-                    {"name": "mta_tax", "type": "FLOAT", "mode": "NULLABLE"},
-                    {"name": "tip_amount", "type": "FLOAT", "mode": "NULLABLE"},
-                    {"name": "tolls_amount", "type": "FLOAT", "mode": "NULLABLE"},
-                    {"name": "improvement_surcharge", "type": "FLOAT", "mode": "NULLABLE"},
-                    {"name": "total_amount", "type": "FLOAT", "mode": "NULLABLE"},
-                    {"name": "congestion_surcharge", "type": "FLOAT", "mode": "NULLABLE"},
-                ]
-            },
-            "externalDataConfiguration": {
-                "sourceFormat": "PARQUET",
-                "compression": "GZIP",
-                "sourceUris": [f"gs://{BUCKET}/raw/{parquet_file}"],
-                #"csvOptions": {"skipLeadingRows": 1},
-            },
-        },
-        bucket=BUCKET,
-        #source_objects=[DATA_SAMPLE_GCS_OBJECT_NAME],
-    )
-
+    
     download_dataset_task >> format_to_parquet_task >> local_to_gcs_task >> bigquery_external_table_task
-
-    """
-    VendorID	tpep_pickup_datetime	tpep_dropoff_datetime	passenger_count	trip_distance	RatecodeID	store_and_fwd_flag	PULocationID	
-    DOLocationID	payment_type	fare_amount	extra	mta_tax	tip_amount	tolls_amount	improvement_surcharge	total_amount	congestion_surcharge
-
-    """
